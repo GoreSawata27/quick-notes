cfefd34c983b8d25617bd38406a33cba1e989a17
Sawata Gore <sawata.gore@dtskill.com>
2025-05-31 11:36:03 +0530

    Variables in js

---
+++ js/Variables.js
@@ -0,0 +1,11 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+// var ->  functional scope -> can reDeclare -> can reassign -> can declare without initialization
+// let ->  block scope -> can reDeclare -> can reassign -> can declare without initialization
+// const ->block scope -> can't reDeclare -> can't reassign -> can't declare without initialization
