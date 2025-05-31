68a89f6d3b8dd31c01141c996064d272f42e2877
Sawata Gore <sawata.gore@dtskill.com>
2025-05-31 13:40:26 +0530

    jsx

---
+++ ReactJs/jsx.js
@@ -0,0 +1,9 @@
+// What is JSX?
+// JSX (JavaScript XML) is a syntax extension for JavaScript used primarily with React.
+// It allows you to write HTML-like code inside JavaScript,
+// making UI structure more readable and expressive.
+
+//  No, the browser does not understand JSX directly.
+
+//  JSX needs to be transpiled (converted) to plain JavaScript using a
+//  tool like Babel before it can run in the browser.
