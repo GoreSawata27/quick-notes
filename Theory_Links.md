4aa546f6aa21c6d54ab5438ced35b7d004a14a68
Sawata Gore <sawata.gore@dtskill.com>
2025-05-31 21:06:46 +0530

    other theory

---
+++ Theory_Links.md
@@ -0,0 +1,5 @@
+ReactJS Interview Questions
+
+https://askreferral.io/interview-questions/reactjs-developer-infosys#:~:text=Can%20you%20walk%20me%20through%20how%20you%20would%20approach%20designing,how%20it%20differs%20from%20props?
+
+https://codersdaily.in/blog/infosys-reactjs-interview-questions-and-answers
---
+++ js/JS_Theory.js
@@ -0,0 +1,13 @@
+// Q => What is node_modules in React?
+// node_modules is a directory that stores all the project’s dependencies installed
+// via npm (Node Package Manager) or yarn.
+
+// Q => What is createRoot in JavaScript (React)?
+// In React 18+, createRoot is a method from the react-dom/client package that initializes
+// a React root for your app using the new concurrent rendering engine.
+// createRoot is the modern way to start a React app in React 18+.
+// It enables advanced React features and better performance.
+// Replaces ReactDOM.render.
+
+// react theory
+// https://codersdaily.in/blog/infosys-reactjs-interview-questions-and-answers
